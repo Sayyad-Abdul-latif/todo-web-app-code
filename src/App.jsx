df77464cea883c0b853e4b8411c6a2e1f61cb671
SAYYAD ABDUL LATIF <108394503+Sayyad-Abdul-latif@users.noreply.github.com>
2022-10-23 09:22:30 +0000

    checkenter function is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,38 @@
 import './App.css'
-import {useState} from 'react';
- function App() {
-   const [task , settask] = useState('')
-   const [todos , settodo] = useState(['Homework' , 'shopping'])
+import { useState } from 'react';
+function App() {
+  const [task, settask] = useState('')
+  const [todos, settodo] = useState([])
 
-   const update = () =>{
-     settodo(todos => {
-       return [...todos , task];
-     });
-     settask('');
-   }
-   const del = () =>{
-    if(todos.length >0){
-      todos.pop()
+  const update = () => {
+    settodo(todos => {
+      return [...todos, task];
+    });
+    settask('');
+  }
+  const del = () => {
+    settodo(todos => {
+      return todos.slice(0, todos.length - 1)
+    })
+  }
+  const checkenter = (e) =>{
+    if(e.keyCode == 13){
+       update();
     }
-     settodo(todos);
-   }
+  }
   return (
     <>
       <h1>Todo App</h1>
-      <input type="text" value = {task} onChange = {event => settask(event.target.value)} ></input>
+      <input onKeyDown={checkenter} type="text" value={task} onChange={event => settask(event.target.value)} ></input>
       <button onClick={update}>click me</button>
-      
+
       <ul>
-        {todos.map(todo =>{
-            return <li>{todo}</li>
+        {todos.map(todo => {
+          return <li>{todo}</li>
         })}
       </ul>
       <button onClick={del}>delete task</button>
     </>
-      )
+  )
 }
 export default App;
